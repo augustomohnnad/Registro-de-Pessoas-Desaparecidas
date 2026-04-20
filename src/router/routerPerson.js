de5c7dd7c2383b525e4de0e48388854b3eab4f21
Augusto Mohannad Silveira <augustomohnnad@gmail.com>
2026-04-20 22:09:05 +0100

    Ajuste da Rota para Upload de imagem

--- src/router/routerPerson.js
+++ src/router/routerPerson.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const multer = require('multer');
+
+
 
 module.exports = (controller) => {
     const router = express.Router()
+    const storage = multer.memoryStorage();
+    const upload = multer({ storage: storage });
 
-    router.post("/", (req, res) => (controller.registerMissing(req, res)))
+    router.post("/", upload.single('photo_url'), (req, res) => (controller.registerMissing(req, res)))
     router.get("/", (req, res) => (controller.everyonePerson(req, res)))
     
     return router

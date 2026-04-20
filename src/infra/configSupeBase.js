2642935a0cc680e572286f334f02fd87d282f17b
Augusto Mohannad Silveira <augustomohnnad@gmail.com>
2026-04-20 22:07:42 +0100

    Ajuste na configuração do Clod Storage

--- src/infra/configSupeBase.js
+++ src/infra/configSupeBase.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
+
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase URL ou Key não encontradas no arquivo .env");
 }
 
-
 const supabase = createClient(supabaseUrl, supabaseKey);
-
 
 module.exports = supabase;
--- src/services/storegeService.js
+++ src/services/storegeService.js
@@ -3,40 +3,36 @@ class StorageService {
         this.supabase = supabaseClient;
     }
 
-    /**
-     * @param {Object} file - Objeto de arquivo vindo do Multer (req.file)
-     * @returns {Promise<string>} - Retorna a URL pública da imagem
-     */
-    async uploadImage(file) {
+    uploadImage = async(file) => {
         try {
-            // Geramos um nome único usando timestamp para evitar que fotos com o mesmo nome se sobrescrevam
-            const fileName = `${Date.now()}-${file.originalname}`;
+            //Acrecentamos o nome do arquivo o TimeStam (época Unix) ex: 123456-photName.jpg
+            const fileName = `${Date.now()}-${file.originalname}`; 
             const filePath = `${fileName}`;
 
-            // 1. Fazemos o upload para o Bucket 'avatars' (que criamos no painel do Supabase)
+            // 1. Fazemos o upload para o Bucket (que criei no  Supabase)
             const { data, error } = await this.supabase.storage
-                .from('img_person')
+                .from('VaiNaWeb')
                 .upload(filePath, file.buffer, {
                     contentType: file.mimetype,
-                    upsert: false // Não sobrescreve arquivos existentes
+                    upsert: false
                 });
 
-            if (error) {
+            if (!error) {
                 throw new Error(`Falha no upload para o Storage: ${error.message}`);
             }
 
             // 2. Solicitamos a URL pública do arquivo que acabamos de subir
             const { data: { publicUrl } } = this.supabase.storage
-                .from('img_person')
+                .from('VaiNaWeb')
                 .getPublicUrl(filePath);
 
-            return publicUrl;
+            return resizeBy.statur(200).json(publicUrl)
 
         } catch (error) {
             console.error("[STORAGE SERVICE ERROR]:", error.message);
-            throw error; // Repassamos o erro para o Controller tratar
+            throw error;
         }
     }
 }
 
-module.exports = StorageService;
+module.exports = StorageService; 

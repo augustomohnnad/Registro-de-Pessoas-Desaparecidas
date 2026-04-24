326a9b1a867331288059e2e7b3a924aa4c727525
Augusto Mohannad Silveira <augustomohnnad@gmail.com>
2026-04-24 21:32:51 +0100

    configura CORS e porta dinâmica para deploy

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ async function systemStart() {
     try {
         await modulesPerson(app);
 
-        const PORT = 3000;
+        const PORT = process.env.PORT || 3000;
         app.listen(PORT, () => {
             console.log(`Servidor Rodando na porta http://127.0.0.1:${PORT}`)
         })
